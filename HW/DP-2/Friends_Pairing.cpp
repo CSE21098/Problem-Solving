97dea5fa6489aa47926d7006e98308ec7d0ec017
Harsh Mishra <96134058+CSE21098@users.noreply.github.com>
2024-05-07 16:49:21 +0530

    Update Friends_Pairing.cpp

--- HW/DP-2/Friends_Pairing.cpp
+++ HW/DP-2/Friends_Pairing.cpp
@@ -13,9 +13,36 @@ private:
         }
         return dp[n];
     }
+    long long  solve(int n , vector<int>& dp){ 
+        
+        if(n==1)
+            return 1;
+        if(n==2)
+            return 2;
+        if(dp[n]!=-1)
+            return dp[n];
+        
+        int r = solve(n-1, dp)*1%mod + ((n-1)*(solve(n-2, dp)%mod))%mod;
+        return  dp[n]=r%mod;   
+    }
+    int solveTab(int n){ 
+        vector<long long> dp(n+1,0);
+        dp[1]=1;
+        dp[2]=2;
+        
+        for(int i=3;i<=n;i++){
+            long a=dp[i-1]%mod;
+            long b=((i-1)*(dp[i-2]%mod))%mod;
+            dp[i]=(a+b)%mod;
+        }
+        return dp[n];
+    }
     
 public:
     int countFriendsPairings(int n){
-        return solve(n);
+        // return solve(n);
+        // vector<int>dp(n+1, -1);
+        // return solve(n , dp);
+        return solveTab(n);
     }
-}; 
+};   
