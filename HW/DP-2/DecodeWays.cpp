9f837fb06b0e0b6b9ddfa3b96bbc2ef3b5ab3e3b
Harsh Mishra <96134058+CSE21098@users.noreply.github.com>
2024-05-07 16:55:40 +0530

    Update DecodeWays.cpp

--- HW/DP-2/DecodeWays.cpp
+++ HW/DP-2/DecodeWays.cpp
@@ -1,27 +1,97 @@
 class Solution {
-public:
-    vector<int> dp;
-    int sizeString;
-    int dfs(int i, string s){
-        if(i == sizeString)
-            return 1;
-        if(i > sizeString || s[i] == '0')
+private:
+    int solve(string &s, int ind, map<string, char> &m) {
+        if(s[ind] == '0') 
             return 0;
-        if(dp[i] != -1)
-            return dp[i];
-        int res = 0;
-        res += dfs(i+1, s);
-        bool check1 = (s[i] == '1') ? true : false;
-        bool check2 = (s[i] == '2' && s[i+1] <= 54) ? true : false;
-        if(check1 | check2)
-            res += dfs(i+2, s);
-        dp[i] = res;
-        return res;
+        if(ind == s.length()-1) 
+            return 1;
+        if(ind == s.length()) 
+            return 1;
+        int takeOne = solve(s, ind+1, m);
+        int takeTwo = 0;
+        string str = "";
+        str.push_back(s[ind]);
+        str.push_back(s[ind+1]);
+        if(m.find(str) != m.end()) 
+            takeTwo = solve(s, ind+2, m);
+        return takeOne + takeTwo;
     }
 
-    int numDecodings(string s) {
-        sizeString = s.size();
-        dp.resize(sizeString+1, -1);
-        return dfs(0, s);   
+    int solveMem(string &s, int ind, map<string, char> &m, vector<int> &dp) {
+        if(s[ind] == '0')
+            return 0;
+        if(ind == s.length()-1) 
+            return 1;
+        if(ind == s.length()) 
+            return 1;
+        if(dp[ind] != -1) 
+            return dp[ind];
+        int takeOne = solveMem(s, ind+1, m, dp);
+        int takeTwo = 0;
+        string str = "";
+        str.push_back(s[ind]);
+        str.push_back(s[ind+1]);
+        if(m.find(str) != m.end()) 
+            takeTwo = solveMem(s, ind+2, m, dp);
+        return dp[ind] = takeOne + takeTwo;
+    }
+
+    int solveTab(string &s, map<string, char> &m) {
+        vector<int> dp(s.length()+1, 0);
+        if(s[s.length()-1] != '0') dp[s.length()-1] = 1;
+        dp[s.length()] = 1;
+        for(int i = s.length()-2; i >= 0; i--) {
+            if(s[i] != '0') {
+                int takingOne = dp[i+1];
+                int takingTwo = 0;
+                string str = "";
+                str.push_back(s[i]);
+                str.push_back(s[i+1]);
+                if(m.find(str) != m.end()) takingTwo = dp[i+2];
+                dp[i] = takingOne + takingTwo; 
+            }
+        }
+        return dp[0];
+    }
+
+    int solveSO(string &s, map<string, char> &m) {
+        int next = 0;
+        if(s[s.length()-1] != '0' && s[0] != '0') next = 1;
+        int nextAfterOne = 1;
+
+        for(int i = s.length()-2; i >= 0; i--){
+            if(s[i] != '0'){
+                int takingOne = next;
+                int takingTwo = 0;
+                string str = "";
+                str.push_back(s[i]);
+                str.push_back(s[i+1]);
+                if(m.find(str) != m.end()) takingTwo = nextAfterOne;
+                int curr = takingOne + takingTwo; 
+                nextAfterOne = next;
+                next = curr;
+            }
+            else{
+                nextAfterOne = next;
+                next = 0;
+            }
+        }
+        return next;
+    }
+
+public:
+    int numDecodings(string s){
+        int ind = 0;
+        map<string, char> m;
+
+        for(int i = 0; i < 26; i++){
+            string str = to_string(i+1);
+            m[str] = 'A'+i;
+        }
+        // return solve(s, ind, m);
+        // vector<int> dp(s.length()+1, -1);
+        // return solveMem(s, ind, m, dp);
+        // return solveTab(s, m);
+        return solveSO(s, m);
     }
 };
