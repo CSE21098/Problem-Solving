9bb8badab043dccd2aa4aedda86710f4fb890e81
Harsh Mishra <96134058+CSE21098@users.noreply.github.com>
2024-05-07 16:44:32 +0530

    Update Brainpower.cpp

--- HW/DP-1/Brainpower.cpp
+++ HW/DP-1/Brainpower.cpp
@@ -1,18 +1,28 @@
 class Solution {
 public:
-    long long fun(int ind,int n,vector<vector<int>>& question,vector<long long>&dp){
-            if(ind>=n) 
-                return 0;
-            if(dp[ind]!=-1) 
-                return dp[ind];
-            long long take=0,nottake=0;
-            take=question[ind][0]+fun(ind+question[ind][1]+1,n,question,dp);
-            nottake=fun(ind+1,n,question,dp);
-            return dp[ind]=max(take,nottake);
+    long long solveMem(int ind,vector<vector<int>>& questions,vector<long long>&dp){
+        if(ind>=questions.size())
+            return 0;
+        if(dp[ind]!=-1)
+            return dp[ind];
+        long long notTake=solveMem(ind+1,questions,dp);
+        long long take=questions[ind][0]+solveMem(ind+questions[ind][1]+1,questions,dp);
+        return dp[ind]=max(take,notTake);
+    }
+    long long solveTab(vector<vector<int>>& questions) {
+        int n=questions.size();
+        vector<long long>dp(n+1,0);
+        for(int ind=n-1;ind>=0;ind--){
+            int jump = min(n, ind+questions[ind][1]+1);
+            long long notTake=dp[ind+1];
+            long long take=questions[ind][0]+dp[jump];
+            dp[ind]=max(notTake,take);
+        }
+        return dp[0];
     }
     long long mostPoints(vector<vector<int>>& questions) {
-        int n=questions.size();
-        vector<long long>dp(n+1,-1);
-        return fun(0,n,questions,dp);
+        // vector<long long>dp(questions.size()+1,-1);
+        // return solveMem(0,questions,dp);
+        return solveTab(questions);
     }
 };
